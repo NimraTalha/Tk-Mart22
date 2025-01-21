79fdd05d595ca3c4d2edf081636aef08f3d81c74
Nimra Talha <nimratalha@gmail.com>
2025-01-21 20:28:42 +0500

    update category.ts

--- sanity/schemas/category.ts
+++ sanity/schemas/category.ts
@@ -1,45 +1,48 @@
 import { defineType, defineField } from "sanity";
 
 export const category = defineType({
-    name: 'category',
-    title: 'Category',
-    type: 'document',
-    fields: [
-        defineField({
-            name: 'name',
-            title: 'Name',
-            type: 'string'
-        }),
-        {
-            name: 'description',
-            title: 'Description',
-            type: 'text'
-        },
-        {
-            name: 'image',
-            title: 'Image',
-            type: 'image',
-            options: {hotspot:true}
-        },
-        {
-            name: 'slug',
-            title: 'Slug',
-            type: 'slug',
-            options: {
-              source: 'name', // Generate slug from the category name
-              maxLength: 96,
-            },
-            validation: Rule => Rule.required().custom(slug => {
-              return !slug || /^[a-z0-9]+(?:-[a-z0-9]+)*$/.test(slug.current)
-                ? true
-                : 'Slug must be lowercase and hyphenated';
-            })
-        },
-        {
-            name: 'parentCategory',
-            title: 'Parent Category',
-            type: 'reference',
-            to: [{ type: 'category' }]  // Allow reference to another category (parent-child structure)
-        },
-    ]
+  name: 'category',
+  title: 'Category',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: { hotspot: true },
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name', // Generate slug from the category name
+        maxLength: 96,
+      },
+      validation: Rule => Rule.required().custom(slug => {
+        if (slug && slug.current) {
+          return /^[a-z0-9]+(?:-[a-z0-9]+)*$/.test(slug.current)
+            ? true
+            : 'Slug must be lowercase and hyphenated';
+        }
+        return true;
+      }),
+    }),
+    defineField({
+      name: 'parentCategory',
+      title: 'Parent Category',
+      type: 'reference',
+      to: [{ type: 'category' }],  // Allow reference to another category (parent-child structure)
+    }),
+  ],
 });
