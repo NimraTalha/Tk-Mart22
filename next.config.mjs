d8b28883cf596eed8f09ea73dc286952e7ee6093
Nimra Talha <nimratalha@gmail.com>
2025-01-21 20:21:58 +0500

    all files and code

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,8 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'images.unsplash.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'cdn.sanity.io',
-        },
-      ],
-    },
-    env: {
-      NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-      NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
-      NEXT_PUBLIC_SANITY_API_VERSION: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-07-07',
-    },
-  };
-  
-  export default nextConfig;
-  
+        domains:[ "cdn.sanity.io"]
+    }
+};
+
+export default nextConfig;
